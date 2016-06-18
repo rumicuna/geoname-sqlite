d04487ac718587f8cd8cb7bfa3b1ca5bfe6ec49d
crazycatdevs <crazycat@c-p-f.org>
2016-06-18 16:12:54 +0200

    Splitting files to avoid out of memory

--- geocharger.sh
+++ geocharger.sh
@@ -13,8 +13,7 @@ if [[ -z $( type -p sqlite3 ) ]]; then echo -e "REQUIRED: sqlite3 -- NOT INSTALL
 # Country selection and download
 echo "Enter the Country code (2 chars) you want to import"
 echo "Or leave empty to import ALL countries"
-echo "***WARNING*** Using ALL countries is heavy !"
-read -p "Country to import ? [All] :" country
+read -p "Country to import ? [All] : " country
 case ${country^^} in
   [A-Z][A-Z] ) COUNTRY="${country^^}";;
   "" ) COUNTRY="allCountries";;
@@ -44,7 +43,7 @@ fi
 
 # Are postal codes required ?
 while true; do
-  read -p "Do you want the postal codes ? [y/N]" getpos
+  read -p "Do you want the postal codes ? [y/N] : " getpos
   case $getpos in
     [yY] ) withPostal=yes; break;;
     [nN] ) withPostal=no; break;;
@@ -60,13 +59,13 @@ then
     echo "Unable to find ${COUNTRY}, sorry..."
     rm -f "postal_${COUNTRY}.zip"
     withPostal=no
-    exit
   fi
 fi
 
 ###############################################
 # Extractions from zip files
 ###############################################
+
 echo "Extracting geodatas..."
 unzip -qq -o "${COUNTRY}.zip" "${COUNTRY}.txt"
 mv "${COUNTRY}.txt" "geo_${COUNTRY}.txt"
@@ -86,7 +85,7 @@ fi
 if [ -f geoloc.sqlite ]
 then
   while true; do
-    read -p "The database file already exists, overwriting ? [y/N]" overwrite
+    read -p "The database file already exists, overwriting ? [y/N] : " overwrite
     case $overwrite in
       [yY] ) rm -rf geoloc.sqlite; break;;
       [nN] ) echo "Ok, so we leave the process..."; exit;;
@@ -95,16 +94,16 @@ then
 fi
 
 echo "Creating and filling geonames table. Please wait"
-sqlite3 geoloc.sqlite "create table geonames (geonameid INTEGER PRIMARY KEY, name TEXT, asciiname TEXT, alternatenames TEXT, latitude DECIMAL(10,7), longitude DECIMAL(10,7), fclass TEXT, fcode TEXT, country TEXT, cc2 TE$
+sqlite3 geoloc.sqlite "create table geonames (geonameid INTEGER PRIMARY KEY, name TEXT, asciiname TEXT, alternatenames TEXT, latitude DECIMAL(10,7), longitude DECIMAL(10,7), fclass TEXT, fcode TEXT, country TEXT, cc2 TEXT, admin1 TEXT, admin2 TEXT, admin3 TEXT, admin4 TEXT, population INTEGER, elevation INTEGER, gtopo30 INTEGER, timezone TEXT, moddate DATETIME);"
 sqlite3 geoloc.sqlite "create index country on geonames(country);"
 sqlite3 geoloc.sqlite "create index names on geonames(name, asciiname, alternatenames);"
-sed -i '/"/""/g' geo_${COUNTRY}.txt
+sed -i 's/"/""/g' geo_${COUNTRY}.txt
 echo -e ".separator \"\t\"\n.import geo_${COUNTRY}.txt geonames" | sqlite3 geoloc.sqlite
 
 if [ ${COUNTRY} = 'allCountries' ]
 then
   echo "Creating and importing countries informations"
-  sqlite3 geoloc.sqlite "create table geocountry (iso_alpha2 TEXT PRIMARY KEY, iso_alpha3 TEXT, iso_numeric INTEGER, fips_code TEXT, name TEXT, capital TEXT, areainsqkm REAL, population INTEGER, continent TEXT, tld TEXT$
+  sqlite3 geoloc.sqlite "create table geocountry (iso_alpha2 TEXT PRIMARY KEY, iso_alpha3 TEXT, iso_numeric INTEGER, fips_code TEXT, name TEXT, capital TEXT, areainsqkm REAL, population INTEGER, continent TEXT, tld TEXT, currency TEXT, currencyName TEXT, Phone TEXT, postalCodeFormat TEXT, postalCodeRegex TEXT, geonameId INTEGER, languages TEXT, neighbours TEXT, equivalentFipsCode TEXT);"
   sqlite3 geoloc.sqlite "create index iso_alpha3 on geocountry(iso_alpha3);"
   sed -i '/^#/d' countryInfo.txt
   echo -e ".separator \"\t\"\n.import countryInfo.txt geocountry" | sqlite3 geoloc.sqlite
@@ -113,11 +112,11 @@ fi
 if [ $withPostal = yes ]
 then
   echo "Creating and filling postal codes. Please wait"
-  sqlite3 geoloc.sqlite "create table geozip (country TEXT, zipcode TEXT, name TEXT, statename TEXT, statecode TEXT, countyname TEXT, countycode TEXT, communame TEXT, commucode TEXT, latitude DECIMAL(10,7), longitude DE$
+  sqlite3 geoloc.sqlite "create table geozip (country TEXT, zipcode TEXT, name TEXT, statename TEXT, statecode TEXT, countyname TEXT, countycode TEXT, communame TEXT, commucode TEXT, latitude DECIMAL(10,7), longitude DECIMAL(10,7), accuracy INTEGER);"
   sqlite3 geoloc.sqlite "create index zipcode on geozip(zipcode);"
-  sqlite3 geoloc.sqlite "create index country on geozip(country);"
-  sqlite3 geoloc.sqlite "create index name on geozip(name);"
-  sed -i '/"/""/g' postal_${COUNTRY}.txt
+  sqlite3 geoloc.sqlite "create index zipcountry on geozip(country);"
+  sqlite3 geoloc.sqlite "create index zipnames on geozip(name);"
+  sed -i 's/"/""/g' postal_${COUNTRY}.txt
   echo -e ".separator \"\t\"\n.import postal_${COUNTRY}.txt geozip" | sqlite3 geoloc.sqlite
 fi
 
@@ -127,8 +126,8 @@ echo "All is filled for $COUNTRY"
 # Cleaning datas for smaller database ?
 ###############################################
 
-read -p "Type N if you don't want to reduce the geonames to cities" reduce
-if [ $reduce != N ]
+read -p "Type N if you don't want to reduce the geonames to cities " reduce
+if [ "$reduce" != "N" ]
 then
   sqlite3 geoloc.sqlite "delete from geonames where fclass<>'P';"
 fi
